5ec3e57f000bb4a0ba75d3b9265963b1bea1047f
crccheck <c@crccheck.com>
2014-02-13 17:58:47 -0600

    pull in orders

---
+++ main.py
@@ -0,0 +1,87 @@
+"""
+references:
+https://github.com/googleads/googleads-python-lib/blob/master/examples/adspygoogle/dfp/v201311/order_service/get_orders_by_statement.py
+"""
+import datetime
+import logging
+import os
+
+from adspygoogle.dfp.DfpClient import DfpClient
+import pytz
+
+
+HOME = os.path.expanduser('~')
+
+
+class Order(object):
+    """
+    advertiserId: str(int) ex: '12345'
+    creatorId: str(int) ex: '12345'
+    currencyCode: str ex: 'USD'
+    endDateTime: datetime
+    externalOrderId: str(int) ex: '0'
+    id: str(int) ex: '12345'
+    isArchived: bool
+    lastModifiedByApp: str ex: 'Goog_DFPUI'
+    lastModifiedDateTime: datetime
+    name: str ex: '1 for All Campaign'
+    notes: str
+    poNumber
+    startDateTime: datetime
+    status:
+    totalBudget:
+        currencyCode: 'USD'
+        microAmount: '0'
+    totalClicksDelivered: int
+    totalImpressionsDelivered: int
+    traffickerId: str(int)
+    unlimitedEndDateTime: bool
+
+    """
+    def __init__(self, order_data):
+        self._original_data = order_data  # hang onto the original data
+        self.__dict__ = order_data.copy()
+
+        # begin type conversions
+
+        # bool
+        self.isArchived = self.isArchived == 'true'
+        self.unlimitedEndDateTime = self.unlimitedEndDateTime == 'true'
+
+        # int
+        self.totalClicksDelivered = int(order_data['totalClicksDelivered'])
+        self.totalImpressionsDelivered = int(order_data['totalImpressionsDelivered'])
+
+        # datetimes
+        if 'endDateTime' in order_data:
+            self.endDateTime = self.to_datetime(order_data['endDateTime'])
+        self.lastModifiedDateTime = self.to_datetime(order_data['lastModifiedDateTime'])
+        self.startDateTime = self.to_datetime(order_data['startDateTime'])
+
+    def __repr__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return self.name.encode('utf8')
+
+    # utilities
+    def to_datetime(self, data):
+        try:
+            tz = pytz.timezone('timeZoneID')
+        except pytz.UnknownTimeZoneError as e:
+            logger = logging.getLogger(__name__)
+            logger.warn(e)
+            tz = None
+        timetuple = (data['date']['year'], data['date']['month'], data['date']['day'], data['hour'], data['minute'], data['second'])
+        timetuple = map(int, timetuple)
+        return datetime.datetime(*timetuple, tzinfo=tz)
+
+
+if __name__ == '__main__':
+    client = DfpClient(path=HOME)
+
+    inventory_service = client.GetService('OrderService', version='v201311')
+
+    statement = {'query': 'LIMIT 500'}
+    results = inventory_service.GetOrdersByStatement(statement)[0]['results']
+    orders = [Order(x) for x in results]
